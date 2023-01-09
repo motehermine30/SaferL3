6deaffa4ba659a908e4134692badf89fc1356020
thiers21 <thierry.ndri85@gmail.com>
2023-01-08 22:46:53 -0800

    ajout image

--- src/Entity/Bien.php
+++ src/Entity/Bien.php
@@ -7,8 +7,11 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: BienRepository::class)]
+#[Vich\Uploadable]
 class Bien
 {
     #[ORM\Id]
@@ -32,7 +35,6 @@ class Bien
     private ?string $ville = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\Regex("/^[0-9]{5}$/")]
     private ?string $code_postal = null;
 
     #[ORM\Column]
@@ -51,9 +53,20 @@ class Bien
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $created_at = null;
 
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $updatedAt = null;
+
+     // NOTE: This is not a mapped field of entity metadata, just a simple property.
+     #[Vich\UploadableField(mapping:'biens', fileNameProperty: 'imageName')]
+     private ?File $imageFile = null;
+ 
+     #[ORM\Column(type: 'string')]
+     private ?string $imageName = null;
+
     public function __construct()
     {
         $this->created_at = new \DateTime();
+        $this->updatedAt = new \DateTime();
     }
 
     public function getSlug(): string {
@@ -186,6 +199,53 @@ class Bien
         $this->created_at = $created_at;
 
         return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     
--- src/Form/BienType.php
+++ src/Form/BienType.php
@@ -3,25 +3,33 @@
 namespace App\Form;
 
 use App\Entity\Bien;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\HttpFoundation\File\File;
 class BienType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('titre')
-            ->add('numero')
-            ->add('surface')
+            ->add('numero', NumberType::class)
+            ->add('surface', NumberType::class)
             ->add('ville')
             ->add('code_postal')
-            ->add('prix_mensuel')
-            ->add('prix_final',Integer::class,[
+            ->add('prix_mensuel', NumberType::class)
+            ->add('prix_final',NumberType::class,[
                 'label'=>'Prix Final'
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG ou PNG)',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer',
+                'download_uri' => false
             ])
             ->add('descriptif',TextareaType::class,[
                 'attr'=>[
